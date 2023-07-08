4bd1342c5ede5bdd2b9f64321fb3a82fa43fd007
Hritik Singh <98538585+Hritik-Singh067@users.noreply.github.com>
2023-07-08 22:37:11 +0530

    First Commit
    Added main files

---
+++ index.js
@@ -0,0 +1,65 @@
+
+var noOfDrums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < noOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var button = this.innerHTML;
+        makeSound(button);
+        buttonAnimation(button);
+    });
+}
+
+document.addEventListener("keypress", function (event) {
+
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(button) {
+
+    switch (button) {
+        case "w":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        default:
+            console.log(button);
+            break;
+    }
+}
+
+function buttonAnimation (button){
+     
+    var activeButton = document.querySelector('.'+button);
+    activeButton.classList.add('pressed');
+
+    setTimeout(function(){
+        activeButton.classList.remove('pressed');
+    }, 100)
+
+}
